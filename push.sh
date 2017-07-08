bdc35f9feee6a87e5348809e6a811a7c45fa4344
Phus Lu <phuslu@hotmail.com>
2017-07-08 14:27:02 +0800

    grep -v push
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-ls -phl | awk 'NR>1' | grep -v -w 'index.html' | sort -k1 -k2 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s-%s %s %s\n", $9, $9, $7, $6, $8, $5)} END{print "</pre></body></html>"}' >index.html 
+ls -phl | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | sort -k1 -k2 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s-%s %s %s\n", $9, $9, $7, $6, $8, $5)} END{print "</pre></body></html>"}' >index.html 
 
 cat autoindex.html >>index.html
 
